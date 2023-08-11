55d36a11b786850e6606193a6603dcb5b9edc184
Kevin Handy <kevin@handy-fx.com>
2023-08-11 16:16:43 -0400

    updates for changing notation and octave display

--- keyboard-demo/script.js
+++ keyboard-demo/script.js
@@ -1,20 +1,33 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key"),
-volumeSlider = document.querySelector(".volume-slider input"),
-keysCheckbox = document.querySelector(".keys-checkbox input");
-octavesCheckbox = document.querySelector(".octaves-checkbox input");
+    volumeSlider = document.querySelector(".volume-slider input"),
+    keysCheckbox = document.querySelector(".keys-checkbox input"),
+    octavesCheckbox = document.querySelector(".octaves-checkbox input"),
+    notationSelection = document.querySelector(".notation-selection");
 
-let allKeys = [],
-audio = new Audio(`tunes/a.wav`); // by default, audio src is "a" tune
+const keys = {
+    'c-major': {
+        'letters': ['C', 'C&sharp;<br>D&flat;', 'D', 'D&sharp;<br>E&flat;', 'E', 'F', 'F&sharp;<br>G&flat;', 'G', 'G&sharp;<br>A&flat;', 'A', 'A&sharp;<br>B&flat;', 'B', 'C', 'C&sharp;<br>D&flat;', 'D', 'D&sharp;<br>E&flat;', 'E', 'F', 'F&sharp;<br>G&flat;', 'G', 'G&sharp;<br>A&flat;', 'A', 'A&sharp;<br>B&flat;', 'B'],
+        'solfege': ['do', 'di<br>ra', 're', 'ri<br>me', 'mi', 'fa', 'fi<br>re', 'sol', 'si<br>le', 'la', 'li<br>te', 'ti', 'do', 'di<br>ra', 're', 'ri<br>me', 'mi', 'fa', 'fi<br>re', 'sol', 'si<br>le', 'la', 'li<br>te', 'ti'],
+        'scale-degree': ['1', '&sharp;1<br>&flat;2', '2', '&sharp;2<br>&flat;3', '3', '4', '&sharp;4<br>&flat;5', '5', '&sharp;5<br>&flat;6', '6', '&sharp;6<br>&flat;7', '7', '8', '&sharp;8<br>&flat;9', '9', '&sharp;9<br>&flat;10', '10', '11', '&sharp;11<br>&flat;12', '12', '&sharp;12<br>&flat;13', '13', '&sharp;13<br>&flat;14', '14']
+    }
+}
+
+let allKeys = [];
+
+const synth = new Tone.Synth().toDestination();
 
 const playTune = (key) => {
-    audio.src = `tunes/${key}.wav`; // passing audio src based on key pressed 
-    audio.play(); // playing audio
+
+    // trigger the attack immediately
+    synth.triggerAttack(key);
 
     const clickedKey = document.querySelector(`[data-key="${key}"]`); // getting clicked key element
     clickedKey.classList.add("active"); // adding active class to the clicked key element
+
     setTimeout(() => { // removing active class after 150 ms from the clicked key element
         clickedKey.classList.remove("active");
-    }, 150);
+        synth.triggerRelease();
+    }, 250);
 }
 
 pianoKeys.forEach(key => {
@@ -24,7 +37,11 @@ pianoKeys.forEach(key => {
 });
 
 const handleVolume = (e) => {
-    audio.volume = e.target.value; // passing the range slider value as an audio volume
+    //audio.volume = e.target.value; // passing the range slider value as an audio volume
+    // synth.volume = (-10 + (e.target.value * 10));
+    console.log(-10 + e.target.value * 10);
+    var vol = new Tone.Volume(-10 + (e.target.value * 10));
+    synth.chain(vol, Tone.Master);
 }
 
 const showHideKeys = () => {
@@ -33,15 +50,28 @@ const showHideKeys = () => {
 }
 
 const toggleSecondOctave = () => {
+    for (let index=13; index<25; index++) {
+        let keyId = "#key" + index;
+        document.querySelector(keyId).classList.toggle("remove");
+    }
+}
 
+const handleNotationChange = (event) => {
+    let counter = 1;
+    for (let notation of keys['c-major'][event.target.value]) {
+        let keyId = "#key" + counter + " span";
+        document.querySelector(keyId).innerHTML = notation;
+        counter++;
+    }
 }
 
 const pressedKey = (e) => {
     // if the pressed key is in the allKeys array, only call the playTune function
-    if(allKeys.includes(e.key)) playTune(e.key);
+    if (allKeys.includes(e.key)) playTune(e.key);
 }
 
 keysCheckbox.addEventListener("click", showHideKeys);
 octavesCheckbox.addEventListener("click", toggleSecondOctave);
-volumeSlider.addEventListener("input", handleVolume);
+//volumeSlider.addEventListener("input", handleVolume);
+notationSelection.addEventListener("change", handleNotationChange);
 document.addEventListener("keydown", pressedKey);
